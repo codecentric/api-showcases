442e55b371a972612ae4a368ea8a128cace53d3c
Daniel Kocot <danielkocot@icloud.com>
2023-11-07 10:15:22 +0100

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE3MzcxMA/spectral.js?branch_slug=main&token=1c8d4df0-8870-41df-9a7e-36321cb180fd";
+export default { extends: ruleset };
